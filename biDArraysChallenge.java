d6bd328eef2cf99e38960d2d62632d4781f40de4
JoaoMarcosRF <160295889+JoaoMarcosRF@users.noreply.github.com>
2025-07-14 18:24:15 -0300

    change name and some variables names.

---
+++ biDArraysChallenge.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+public class biDArraysChallenge{
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Insira quantos alunos deseja inserir: ");
+        int studantCount = input.nextInt();
+
+        System.out.print("Insira a quantidade notas que cada alunos tem: ");
+        int studantGrades = input.nextInt();
+
+        double[][] studantsGradesArray = new double[studantCount][studantGrades];
+
+        double average = 0;
+
+        //set
+        for(int lines = 0; lines < studantCount; lines++){
+            for(int rows = 0; rows < studantGrades; rows++){
+                System.out.printf("Insira a %d° nota do aluno[%d]: ",rows + 1, lines + 1);
+                studantsGradesArray[lines][rows] = input.nextDouble(); 
+
+                average += studantsGradesArray[lines][rows];
+
+                input.nextLine();// clean buffer
+            }
+        }
+
+        //show
+        for(int lines = 0; lines < studantCount; lines++){
+            for(int rows = 0; rows < studantGrades; rows++){
+                System.out.printf("Nota[%d] do %d° estudante: " + String.format("%.02f",studantsGradesArray[lines][rows]) + "\n", rows + 1, lines + 1);
+            }
+        }
+
+        System.out.printf("A media geral da turma é igual a: %.02f", average / (studantCount * studantGrades));
+
+        input.close();
+    }
+}
